6c48e443bc146fd7e2d3b5743fb235b00070f3d1
ilyapishchulin <shonheldin@mail.ru>
2022-02-23 16:16:10 +0300

    Add bridge listener and app init

--- src/init/ConfigProvider.tsx
+++ src/init/ConfigProvider.tsx
@@ -1,9 +1,34 @@
-import { FC } from 'react';
-import { ConfigProvider as VKUIConfigProvider } from '@vkontakte/vkui';
+import { FC, useEffect, useState, useCallback } from 'react';
+
+import bridge, { AnyReceiveMethodName, AppearanceSchemeType, VKBridgeEvent, VKUpdateConfigData } from '@vkontakte/vk-bridge';
+
+import { ConfigProvider as VKUIConfigProvider, Scheme } from '@vkontakte/vkui';
 import { Adaptive } from './Adaptive';
 
-export const ConfigProvider: FC = () => (
-  <VKUIConfigProvider>
-    <Adaptive />
-  </VKUIConfigProvider>
-);
+export const ConfigProvider: FC = () => {
+  const [scheme, setScheme] = useState<AppearanceSchemeType>(Scheme.BRIGHT_LIGHT);
+
+  const bridgeListener = useCallback(
+    ({ detail: { type, data } }: VKBridgeEvent<AnyReceiveMethodName>) => {
+      if (type !== 'VKWebAppUpdateConfig') {
+        return;
+      }
+
+      setScheme((data as VKUpdateConfigData).scheme);
+    },
+    [],
+  );
+
+  useEffect(() => {
+    bridge.subscribe(bridgeListener);
+    void bridge.send('VKWebAppInit');
+
+    return () => bridge.unsubscribe(bridgeListener);
+  }, []);
+
+  return (
+    <VKUIConfigProvider scheme={scheme}>
+      <Adaptive />
+    </VKUIConfigProvider>
+  );
+};
